dd7bc306198e87f250021998fc8e222ac676a25c
graham <graham.annett@gmail.com>
2021-11-12 17:10:05 -0700

    fgsm stub

--- tellem/attacks/fgsm.py
+++ tellem/attacks/fgsm.py
@@ -1,8 +1,19 @@
-class AttackCore:
-    def __init__(self, *args, **kwargs):
+from typing import Any, Callable
+
+from tellem.implementations import ImplementationBase
+from tellem.types import Model, Tensor
+
+
+class FastGradientSignMethod(ImplementationBase):
+    def __init__(self, model: Model = None, loss_func: Callable[..., Any] = None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.loss_func = loss_func
+
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        return super().__call__(*args, **kwds)
+
+    def generate_attack(self, x: Tensor, y: Tensor, **kwargs):
         pass
 
-
-class FastGradientSignMethod(AttackCore):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __tellem_function__(self, *args, **kwargs):
+        return super().__tellem_function__(*args, **kwargs)
