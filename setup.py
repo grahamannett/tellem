a1516b204cba884d0d639b397fcfa36e976f97ff
graham <graham.annett@gmail.com>
2021-11-18 22:30:12 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup
 setup(
     name='tellem',
     version='0.0.1',
+    author="graham annett",
+    author_email="graham.annett@gmail.com",
+    url="https://github.com/grahamannett/tellem",
     packages=['tellem'],
     install_requires=[
         'numpy',
