2ff677272cfd5381b14e3f30295085dc4b0bfdd7
graham <graham.annett@gmail.com>
2021-12-10 22:32:31 -0700

    fix setup.py for pip install from repo

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
-    name='tellem',
-    version='0.0.1',
+    name="tellem",
+    version="0.0.1",
     author="graham annett",
     author_email="graham.annett@gmail.com",
     url="https://github.com/grahamannett/tellem",
-    packages=['tellem'],
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
     install_requires=[
-        'numpy',
-        'torch',
-        'pytest',
+        "numpy",
+        "torch",
+        "pytest",
     ],
 )
